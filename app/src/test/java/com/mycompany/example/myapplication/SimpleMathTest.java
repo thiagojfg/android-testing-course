63eeb4896907401ff7e90b1c8c19be6a354f7f05
Thiago Luiz Alves <thiagojfg@gmail.com>
2018-02-15 01:37:09 -0200

    implementation of method div and unit tests

--- app/src/test/java/com/mycompany/example/myapplication/SimpleMathTest.java
+++ app/src/test/java/com/mycompany/example/myapplication/SimpleMathTest.java
@@ -2,21 +2,40 @@ package com.mycompany.example.myapplication;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SimpleMathTest {
+
+    private SimpleMath sm;
+
+    @Before
+    public void setup(){
+        sm = new SimpleMath();
+    }
+
     @Test
     public void testAdd() {
-        SimpleMath sm = new SimpleMath();
         int total = sm.add(4, 5);
         assertEquals("Simple Math is not adding correctly", 9, total);
     }
 
     @Test
     public void testDiff() {
-        SimpleMath sm = new SimpleMath();
         int total = sm.diff(9, 2);
         assertEquals("Simple Math is not subtracting correctly", 7, total);
+    }
+
+    @Test
+    public void testDiv(){
+        double total = sm.div(10, 5);
+        assertEquals("Simple math is not dividing correctly", Double.valueOf(2.0), Double.valueOf(total));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivWithZeroDivisor(){
+        double total = sm.div(10, 0);
+        assertEquals("Simple math is not handling division by zero correctly", 0.0, total, 0.0);
     }
 }
 
